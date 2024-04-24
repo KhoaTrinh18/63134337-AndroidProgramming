a9eb310a6e366fb32fe0f17a2257959a6b5e79f5
KhoaTrinh18 <trinhkhoa1811@gmail.com>
2024-04-24 16:27:49 +0700

    Đổi tên ví dụ 1 của Intent

---
+++ Intent_vidu1/app/src/main/java/trinhkhoa/intent_vidu1/SubActivity.java
@@ -0,0 +1,31 @@
+package trinhkhoa.intent_vidu1;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class SubActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_sub);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    public void quayVe(View v){
+        Intent iManHinhChinh = new Intent(this, MainActivity.class);
+        startActivity(iManHinhChinh);
+    }
+}
